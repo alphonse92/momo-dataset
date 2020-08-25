3de83413ea11b051ade252d786b4487146d55bd7
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-08-25 12:10:05 -0500

    adding more classes to the dataset

--- downloadNegativeExamplesDataset.py
+++ downloadNegativeExamplesDataset.py
@@ -5,15 +5,59 @@ from lib import util
 util.createDatasetFolderStructure()
 
 util.downloadImagesFromGoogle(
+    "scary images -momo",
+    "scary_images_",
+    200,
+    downloadpath="raw/scary",
+)
+
+
+util.downloadImagesFromGoogle(
     "meme template",
-    "meme_tmpl",
-    100,
+    "meme_tmpl_",
+    200,
     downloadpath="raw/meme",
 )
 
 util.downloadImagesFromGoogle(
     "person photography",
-    "person_pht",
-    150,
+    "person_pht_",
+    200,
     downloadpath="raw/person",
 )
+
+util.downloadImagesFromGoogle(
+    "sports",
+    "sports_",
+    200,
+    downloadpath="raw/sports",
+)
+
+util.downloadImagesFromGoogle(
+    "news",
+    "news_",
+    200,
+    downloadpath="raw/news",
+)
+
+util.downloadImagesFromGoogle(
+    "night andscapes",
+    "night_landscapes_",
+    200,
+    downloadpath="raw/landscapes",
+)
+
+util.downloadImagesFromGoogle(
+    "trees",
+    "trees_",
+    200,
+    downloadpath="raw/trees",
+)
+
+util.downloadImagesFromGoogle(
+    "montains",
+    "montains_",
+    200,
+    downloadpath="raw/montains",
+)
+
